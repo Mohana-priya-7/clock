3941f286423c4b15ff8c8e06825c607c8c5f3c53
Mohana-priya-7 <168969872+Mohana-priya-7@users.noreply.github.com>
2025-06-27 14:18:10 +0530

    clock

---
+++ index.js
@@ -0,0 +1,55 @@
+// Array of background images
+const images = [
+  "https://plus.unsplash.com/premium_photo-1710011850786-85eb679110bb?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1555750287-affefd650a40?q=80&w=1496&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1742559211610-132a4d3d7d37?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1709733167470-659e3c0fdcc1?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1568141012984-5d9ef4172c00?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+
+let currentIndex = 0;
+
+// Function to change the background image
+function changeBackground() {
+  document.body.style.background = `url('${images[currentIndex]}') no-repeat center center fixed`;
+  document.body.style.backgroundSize = "cover";
+  currentIndex = (currentIndex + 1) % images.length; // Loop back to the first image
+}
+
+// Change background every 2 seconds
+setInterval(changeBackground, 2000);
+
+// Set the initial background
+changeBackground();
+
+// Clock functionality
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
+
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hourEl.innerText = h;
+  minuteEl.innerText = m;
+  secondEl.innerText = s;
+  ampmEl.innerText = ampm;
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
+
+updateClock();
